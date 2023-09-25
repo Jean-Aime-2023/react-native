3a375245ae8d186e896d14dfc1fc2cde5107662f
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-09-25 15:16:42 +0200

    view

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { View } from "react-native";
 
 export default function App(){
-  return <View style={{flex:1,backgroundColor:"plum"}}></View>
-}
+  return <View style={{flex:1,backgroundColor:"plum"}}>
+    <View style={{width:200,height:200,backgroundColor:"lightblue"}}>  
+    </View>
+    <View style={{width:200,height:200,backgroundColor:"lightgreen"}}>
+    </View>
+  </View>
+} 
