fe5020e56e0d85f10f376fa8322735ecbb6624bf
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-09-26 08:46:33 +0200

    multiplestyles

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { View, Alert, Button } from "react-native";
-import Greet from "./components/Greet";
+import { Text,View,StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Greet name="Bruce Wayne"/>
-      <Greet name=" Clark Kent"/>
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <View style={[styles.box,styles.lightblueBg]}><Text>Light blue box</Text></View>
+      <View style={[styles.box,styles.lightgreenBg]}><Text>Light green box</Text></View>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{flex:1,backgroundColor:"plum",padding:60},
+  box:{
+    width:100,
+    height:100,
+    padding:10,
+  },
+  lightblueBg:{
+    backgroundColor:"lightblue",
+  },
+  lightgreenBg:{
+    backgroundColor:"lightgreen",
+  }
+})
